3a6a505fdc4a52bcd38507c071b7d6dee6501dce
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-02-11 17:29:45 -0600

    some design

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,22 +4,24 @@ const Header = (props) => {
 	return (
 		<div>
 			<header className='header'>
-				<div className='nav'>
-					<li className="nav-bar">Navigation
-						<ul>
-							<li><a href="/">Home</a></li>
-							<li><a href="/">Search</a></li>
-							<li><a href="/">Log Out</a></li>
-						</ul>
-					</li>
-				</div>
-				<div>
-					<h1>
-						Where To Eat?
-					</h1>
-				</div>
-				<div>
-					<button onClick={props.goToProfile}>Your Profile</button>
+				<div className='wrap'>
+					<div className='nav'>
+						<li className="nav-bar">Navigation
+							<ul>
+								<li><a href="/">Home</a></li>
+								<li><a href="/">Search</a></li>
+								<li><a href="/">Log Out</a></li>
+							</ul>
+						</li>
+					</div>
+					<div>
+						<h1 className="header-h1">
+							Where To Eat?
+						</h1>
+					</div>
+					<div>
+						<button onClick={props.goToProfile}>Your Profile</button>
+					</div>
 				</div>
 			</header>
 		</div>
