630371d04960747b5f95ca7fb21ad8eae03f7ceb
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-02-13 12:54:22 -0600

    saving works

--- src/Login/index.js
+++ src/Login/index.js
@@ -24,7 +24,7 @@ class Login extends Component {
 	handleSubmit = async (e) => {
 		e.preventDefault()
 		try {
-			const loginResponse = await fetch('http://localhost:9000/auth/login', {
+			const loginResponse = await fetch('http://localhost:9000/api/v1/auth/login', {
 				method: 'POST',
 				credentials: 'include',
 				body: JSON.stringify(this.state),
@@ -67,7 +67,7 @@ class Login extends Component {
 		console.log('here we are')
 		try {
 			console.log('are we getting here or not')
-			const registrationResponse = await fetch('http://localhost:9000/auth/register', {
+			const registrationResponse = await fetch('http://localhost:9000/api/v1/auth/register', {
 				method: 'POST',
 				credentials: 'include',
 				body: JSON.stringify(state),
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,27 +1,64 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './index.css'
 
 class Profile extends React.Component {
 	constructor() {
 		super();
 		this.state = {
+			saved: [],
 			showEdit: false
 		}
 	} 
-	// getSavedRestaurants = () => {
-	// 	this.props.saved.map((restaurants, i) => {
+	componentDidMount() {
+		this.getSavedRestaurants()
+	}
+	getSavedRestaurants = async () => {
+		try {
+			const response = await fetch('http://localhost:9000/api/v1/restaurantsga/returnsaved', {
+				method: 'GET',
+				credentials: 'include'
+			})
+			if (!response.ok) {
+				throw Error(response.statusText)
+			}
+			const parsedResponse = await response.json();
+			this.setState({
+				saved: parsedResponse.data
+			})
+		} catch (err) {
+			console.log(err)
+			return err
+		}
+	} 
+	// mapOverSavedRestaurants = () => {
+	// 	this.state.saved.map((restaurants, i) => {
 	// 		return (
-	// 			<li key={i} className="user-profile-restaurants">
-	// 				{ restaurants }
+	// 			<li key={i}>
+	// 				{restaurants.name}
+	// 				<br />
+	// 				{restaurants.formatted_address}
+	// 				<hr />
 	// 			</li>
 	// 		)
 	// 	})
 	// }
 	render() {
+		// MAP
+		console.log(this.state)
+		const mappedRestaurants = this.state.saved.map((restaurants, i) => {
+			return (
+				<li key={i}>
+					{restaurants.name}
+					<br />
+					{restaurants.formatted_address}
+					<hr />
+				</li>
+			)
+		})
 		return (
 			<div className="profile-wrap">
 				<h1 className="user-profile-title">Your Saved Restaurants</h1>
-				<h3 className="user-profile-restaurants">saved restaurants will print here</h3>
+				<h3 className="user-profile-restaurants">{mappedRestaurants}</h3>
 				<button onClick={this.props.closeProfile}>Back To Search</button>
 			</div>
 		)
