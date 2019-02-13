f2bca6796c7f4846b381bb94e33e742ec0700d1b
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-02-13 12:15:55 -0600

    fixing profile

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,22 +1,31 @@
- import React from 'react';
+ import React, { Component } from 'react';
 import './index.css'
 
-const Profile = (props) => {
-	console.log(props)
-	const getSavedRestaurants = props.saved.map((restaurants, i) => {
+class Profile extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			showEdit: false
+		}
+	} 
+	// getSavedRestaurants = () => {
+	// 	this.props.saved.map((restaurants, i) => {
+	// 		return (
+	// 			<li key={i} className="user-profile-restaurants">
+	// 				{ restaurants }
+	// 			</li>
+	// 		)
+	// 	})
+	// }
+	render() {
 		return (
-			<li key={i} className="user-profile-restaurants">
-				{ restaurants }
-			</li>
+			<div className="profile-wrap">
+				<h1 className="user-profile-title">Your Saved Restaurants</h1>
+				<h3 className="user-profile-restaurants">saved restaurants will print here</h3>
+				<button onClick={this.props.closeProfile}>Back To Search</button>
+			</div>
 		)
-	})
-	return (
-		<div className="profile-wrap">
-			<h1 className="user-profile-title">Your Saved Restaurants</h1>
-			<h3 className="user-profile-restaurants">{getSavedRestaurants}</h3>
-			<button onClick={props.closeProfile}>Back To Search</button>
-		</div>
-	)
+	}
 }
 
 export default Profile;
